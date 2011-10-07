5afbe2218b9d506f28d5355cf404f64ce2ac3aa8
dsisnero <dsisnero@gmail.com>
2011-10-07 12:33:52 -0600

    got rid of tool-bar-help

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/"))))
- '(tool-bar-mode t))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
