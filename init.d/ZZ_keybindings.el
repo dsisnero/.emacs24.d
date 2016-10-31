d3489c071b0ad8b446024fe46fbea79e911cefb8
Dominic Sisneros <dsisnero@gmail.com>
2016-10-31 11:34:22 -0600

    added magit stats C-x g

--- init.d/ZZ_keybindings.el
+++ init.d/ZZ_keybindings.el
@@ -4,7 +4,7 @@
 ;;(global-unset-key (kbd "C-x C-z"))
 
 
-
+(global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "<f8>") 'gist-region-or-buffer)
 (global-set-key (kbd "<f9>") 'org2blog/wp-new-entry)
 (global-set-key (kbd "S-<f9>") 'org2blog/wp-post-buffer)
