fcac6fb0e5bf824d5a1a014a12b6e92468f5d81e
Dominic Sisneros <dsisnero@gmail.com>
2017-03-03 11:22:31 -0700

    more helm config
    bound helm actions to C-z and action to Tab

--- init.d/ZZ_keybindings.el
+++ init.d/ZZ_keybindings.el
@@ -9,3 +9,15 @@
 (global-set-key (kbd "<f9>") 'org2blog/wp-new-entry)
 (global-set-key (kbd "S-<f9>") 'org2blog/wp-post-buffer)
 
+(global-set-key (kbd "M-x") #'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "s-f") #'helm-projectile-ag)
+(global-set-key (kbd "s-t") #'helm-projectifle-finde-file-dwim)
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+
+;;(global-set-key (kbd "C-c-p-p") #'helm-projectile-switch-projct)
